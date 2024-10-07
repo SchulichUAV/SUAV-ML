f6043907bdbc189aaaca35e8c25078e07bbd571a
Dominic Gartner <dominic.gartner@ucalgary.ca>
2024-10-06 18:57:54 -0600

    Global var

--- Utils/Helper.py
+++ Utils/Helper.py
@@ -4,6 +4,27 @@ to keep common functions organized and avoid code duplication.
 '''
 from PIL import Image
 
+# Full list of objects that are available for detection. Dataset labels
+OBJECTS = [
+    'Trash Can',
+    'Tire',
+    'Bike',
+    'Basketball',
+    'Cone',
+    'Mannequin'
+]
+
+# Target objects we need to drop payload on.
+# We only update the JSON if detection is one of these objects.
+# NOTE: UPDATE prior to competition with up-to-date target objects
+TARGET_OBJECTS = [
+
+]
+
+# Universal Image sizes / specs for pre-processing
+IMG_X = 30
+IMG_Y = 30
+
 def process_image(image: Image.Image) -> Image.Image:
     '''
     Pre-processes image to be compatable with models and optimize processing time
@@ -18,3 +39,8 @@ def process_image(image: Image.Image) -> Image.Image:
     PIL.Image.Image: The processed version of the input image
     '''
     pass
+
+def update_position(old_lat: float, old_long: float, new_lat: float, new_long: float) -> tuple[float, float]:
+    updated_lat = (old_lat + new_lat) / 2
+    updated_long = (old_long + new_long) / 2
+    return updated_lat, updated_long
