deb97c5161962d00c58432010447cd9894afd05f
Dominic Gartner <dominic.gartner@ucalgary.ca>
2024-12-07 13:59:23 -0700

    Inference using hosted API (#13)

--- Scripts/Training/YOLO_model.py
+++
@@ -1,8 +0,0 @@
-'''
-Training of YOLO model.
-Saves the trained YOLO model to "Models" folder.
-'''
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../Utils')))
-from Helper import process_image
